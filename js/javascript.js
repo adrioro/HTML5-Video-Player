54e7e37cafa9902a48141029af35deb55c489326
adrioro <artistsvillage@me.com>
2019-02-12 11:37:27 -0500

    Commit 8

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,13 @@
 var myVideo = document.getElementById("video");
+
+function big() {
+    myVideo.width = 854;
+}
+
+function small() {
+    myVideo.width = 320;
+}
+
+function normal() {
+    myVideo.width = 420;
+}
